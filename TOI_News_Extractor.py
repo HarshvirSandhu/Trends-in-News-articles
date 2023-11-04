3015eb88964df169390bb13ff7f83b3a828a008d
Harshvir Sandhu <75773763+HarshvirSandhu@users.noreply.github.com>
2023-11-05 01:15:20 +0530

    Update TOI_News_Extractor.py html class name

--- TOI_News_Extractor.py
+++ TOI_News_Extractor.py
@@ -19,7 +19,7 @@ def fetch_article_content(url):
         response = requests.get(url)
         response.raise_for_status()
         soup = BeautifulSoup(response.text, 'html.parser')
-        article_content = soup.find('div', {'class': 'article_content'})  # Modify this based on the structure of the webpage
+        article_content = soup.find('div', {'class': '_s30J clearfix'})  # Modify this based on the structure of the webpage
         return article_content.get_text() if article_content else None
     except Exception as e:
         print(f"Error fetching article content: {e}")
