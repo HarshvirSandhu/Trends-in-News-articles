e8c556d9c0afd028dceeadb36d8d8fe468f27253
Harshvir Sandhu <75773763+HarshvirSandhu@users.noreply.github.com>
2023-11-04 23:20:52 +0530

    Update TOI_scraper.py
    Removed needless comments and unused variable

--- TOI_scraper.py
+++ TOI_scraper.py
@@ -2,11 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 import pandas as pd
-dd = 11
+
 mm_list = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
-year=2015
 mappings = dict()
-months = []
 skipped = 0
 count = 0
 mm=0
@@ -27,55 +25,6 @@ while count <= 365 and mm<len(mm_list):
         count += 1
     except:
         mm+=1
-        # count-=1
-
-
-# for mm in mm_list:
-#     browser = webdriver.Chrome()
-#     browser.get(f'https://timesofindia.indiatimes.com/archive/year-2015,month-{mm}.cms')
-#     time.sleep(1)
-#     # try:
-#     calender = browser.find_element(By.ID, 'calender')
-#     dates = calender.find_elements(By.TAG_NAME, 'td')
-#     date_indices = [i for i, item in enumerate(dates) if item.text.isnumeric()]
-#     for d in date_indices:
-#         print(dates[d].get_attribute('innerHTML'), '\n\n\n')
-#     print(len(date_indices))
-#     print(calender.text)
-#     for i in date_indices:
-#     # try:
-#         print(i, len(date_indices))
-#         print(browser.current_url)
-#         dates[i].find_element(By.TAG_NAME, 'a').click()
-#         time.sleep(3)
-#         print(browser.current_url)
-#
-#         links = browser.find_elements(By.XPATH,
-#                                       '//*[@style="font-family:arial ;font-size:12;font-weight:bold; color: #006699"]')
-#         # print(links, links[1].text)
-#         print('-------- NEW DATE ----------')
-#         in_html = [j.find_elements(By.TAG_NAME, 'a') for j in links][1]
-#         print(len(in_html), '{{{{{{{{{{{')
-#         for title in in_html:
-#             mappings[title.text] = title.get_attribute('href')
-#         # months.append(mm)
-#         browser.quit()
-#         browser = webdriver.Chrome()
-#         browser.get(f'https://timesofindia.indiatimes.com/archive/year-2015,month-{mm}.cms')
-#         time.sleep(5)
-#             # except Exception as e:
-#             #     print('Skipping a date', browser.current_url)
-#             #     print(e)
-#             #     skipped+=1
-#             #     browser.get(f'https://timesofindia.indiatimes.com/archive/year-2015,month-{mm}.cms')
-#             #     time.sleep(2)
-#     # except Exception as e:
-#     #     print(e)
-#     print('################ NEW MONTH #######################')
-#     browser.back()
-#     time.sleep(2)
-#         # print(mappings)
-#     browser.quit()
 
 df = pd.DataFrame({'Title': list(mappings.keys())})
 df['URL'] = list(mappings.values())
