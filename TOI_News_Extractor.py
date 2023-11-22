ac3cfcf51f45720b88eefc1f7a67e570a2c23c33
Akhil Sharma <akhilsharma.off@gmail.com>
2023-11-22 11:52:23 +0530

    Modifed Files

--- TOI_News_Extractor.py
+++ TOI_News_Extractor.py
@@ -12,21 +12,23 @@ def extract_unique_names(url):
         return match.group(1)
     else:
         return None
-
+count = 0
 # Define a function to fetch and extract article content from a URL
 def fetch_article_content(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
         soup = BeautifulSoup(response.text, 'html.parser')
-        article_content = soup.find('div', {'class': '_s30J clearfix'})  # Modify this based on the structure of the webpage
+        article_content = soup.find('div', {'class': '_s30J clearfix'})  
+        print("done")
         return article_content.get_text() if article_content else None
     except Exception as e:
         print(f"Error fetching article content: {e}")
+        count+=1
         return None
 
 # Input CSV file path
-input_csv_file = 'TOI_2015_data_train.csv'  # Replace with your input CSV file path
+input_csv_file = 'test.csv'  # Replace with your input CSV file path
 
 # Read the existing CSV file into a DataFrame
 df = pd.read_csv(input_csv_file)
@@ -38,3 +40,5 @@ df['article data'] = df['URL'].apply(lambda x: fetch_article_content(x))
 df.to_csv(input_csv_file, index=False)
 
 print(f"'article data' column added and data saved to {input_csv_file}")
+
+print(f'Not able to extract {count}')
